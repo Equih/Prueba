b07c8a032b2cbee67bc502e2521074884b960d50
Leonardo <leo.e.44@gmail.com>
2015-12-09 20:48:57 -0600

    Initial Commit

---
+++ PruebaGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PruebaGit
+//
+//  Created by Leonardo Adolfo Garcia E. on 09/12/15.
+//  Copyright © 2015 Leonardo García. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
